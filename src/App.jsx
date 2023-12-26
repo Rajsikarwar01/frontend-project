f5147d8e083a6913a66c2d90cd555f5f333b5a57
Rajsikarwar01 <124458241+Rajsikarwar01@users.noreply.github.com>
2023-12-26 12:43:29 +0530

    Added basic layout and integrated to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Routes } from 'react-router-dom'
-import './App.css'
+import { Routes } from 'react-router-dom';
+
+import './App.css';
+
+import HomePage from './Pages/HomePage';
 
 function App() {
 
   return (
     <>
-     <Routes>
-     {/* <Route path="/" element={<Home />} ></Route> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} ></Route>
 
-     </Routes>
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import App from './App.jsx'
 import './index.css'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './Redux/slices/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-    <Toaster></Toaster>
-  </BrowserRouter>
-  
+  <Provider store={store}>
+    <BrowserRouter>
+       <App />
+       <Toaster></Toaster>
+    </BrowserRouter>
+  </Provider>
 )
